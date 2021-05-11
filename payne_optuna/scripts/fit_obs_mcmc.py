2ed96051e5fb9faa93f7b65b505369defc00e835
nathansandford <nathan_sandford@berkeley.edu>
2021-05-11 14:37:26 -0700

    bugfix

--- payne_optuna/scripts/fit_obs_mcmc.py
+++ payne_optuna/scripts/fit_obs_mcmc.py
@@ -483,7 +483,7 @@ def main(args):
             rv=ensure_tensor(optim_fit["rv"]),
             vmacro=ensure_tensor(optim_fit["vmacro"]) if optim_fit["vmacro"] is not None else None,
             vsini=None,  # ensure_tensor(optim_fit["vsini"]) if optim_fit["vsini"] is not None else None,
-            inst_res=None if args.resolution == 'default' else args.resolution,  # ensure_tensor(optim_fit["inst_res"]) if optim_fit["inst_res"] is not None else None,
+            inst_res=None if args.resolution == 'default' else int(args.resolution),  # ensure_tensor(optim_fit["inst_res"]) if optim_fit["inst_res"] is not None else None,
             cont_coeffs=ensure_tensor(optim_fit["cont_coeffs"]),
         )
         log_like = (
