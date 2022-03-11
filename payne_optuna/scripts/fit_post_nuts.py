8455a7b62ab58e10c7a7f685385378a9893f1625
nathansandford <nathan_sandford@berkeley.edu>
2022-03-10 18:07:21 -0800

    crude script for posterior analysis

--- payne_optuna/scripts/fit_post_nuts.py
+++ payne_optuna/scripts/fit_post_nuts.py
@@ -1,96 +1,112 @@
+import argparse
 from pathlib import Path
 from tqdm import tqdm
+
 import numpy as np
 import pandas as pd
 import pymc as pm
 
+def parse_args(options=None):
+    """
+    Arg Parser
+    """
+    parser = argparse.ArgumentParser(description="Fit Observed Spectrum w/ Optimizer")
+    parser.add_argument("program", help="Program")
+    if options is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(options)
+    return args
 
-payne_wrkdir = Path('/global/scratch/users/nathan_sandford/payne_wrkdir')
-data_dir = payne_wrkdir.joinpath('data')
-mcmc_summary_file = data_dir.joinpath('mcmc_summary.h5')
+def main(args):
+    payne_wrkdir = Path('/global/scratch/users/nathan_sandford/payne_wrkdir')
+    data_dir = payne_wrkdir.joinpath('data')
+    mcmc_summary_file = data_dir.joinpath('mcmc_summary.h5')
 
-mcmc_df = pd.read_hdf(mcmc_summary_file, 'individual')
-bounds = pd.read_hdf(mcmc_summary_file, 'bounds')
-lower_bounds = bounds.loc['lower_bounds']
-upper_bounds = bounds.loc['upper_bounds']
-label_names = list(lower_bounds.index)
+    mcmc_df = pd.read_hdf(mcmc_summary_file, 'individual')
+    bounds = pd.read_hdf(mcmc_summary_file, 'bounds')
+    lower_bounds = bounds.loc['lower_bounds']
+    upper_bounds = bounds.loc['upper_bounds']
+    label_names = list(lower_bounds.index)
 
-unscaled_samples_dict = np.load(data_dir.joinpath('mcmc_stacked_samples.npz'))
+    unscaled_samples_dict = np.load(data_dir.joinpath('mcmc_stacked_samples.npz'))
 
-stack_df = pd.DataFrame(index=mcmc_df['obs_tag'].unique())
+    stack_df = pd.DataFrame(index=mcmc_df['obs_tag'].unique())
 
-for obs_tag in tqdm(mcmc_df['obs_tag'].unique()):
-    print(obs_tag)
-    for i, label in enumerate(tqdm(label_names)):
-        if label in ['Teff', 'logg']:
-            continue
-        print(i, label)
+    obs_tags = [obs_tag for obs_tag in mcmc_df['obs_tag'].unique() if args.program in obs_tag]
 
-        data = unscaled_samples_dict[obs_tag][:, :, i]
-        n_exp = data.shape[0]
+    for obs_tag in tqdm(obs_tags):
+        print(obs_tag)
+        for i, label in enumerate(tqdm(label_names)):
+            if label in ['Teff', 'logg']:
+                continue
+            print(i, label)
 
-        model = pm.Model()
+            data = unscaled_samples_dict[obs_tag][:, :, i]
+            n_exp = data.shape[0]
 
-        with model:
-            # Priors
-            mu_exp = pm.TruncatedNormal(
-                "mu_exp",
-                mu=np.mean(data, axis=(0, 1)),
-                sigma=upper_bounds[label] - lower_bounds[label],
-                lower=lower_bounds[label],
-                upper=upper_bounds[label],
-                shape=(n_exp,)
-            )
-            mu_stack = pm.TruncatedNormal(
-                "mu_stack",
-                mu=np.mean(data, axis=(0, 1)),
-                sigma=upper_bounds[label] - lower_bounds[label],
-                lower=lower_bounds[label],
-                upper=upper_bounds[label]
-            )
-            sigma_exp = pm.HalfNormal("sigma_exp", 0.2, shape=(n_exp,))
-            sigma_stack = pm.HalfNormal("sigma_stack", 0.2)
-            # Likelihood
-            for j in range(n_exp):
-                theta_exp = pm.TruncatedNormal(
-                    f"theta_exp_{j}",
-                    mu=mu_exp[j],
-                    sigma=sigma_exp[j],
+            model = pm.Model()
+
+            with model:
+                # Priors
+                mu_exp = pm.TruncatedNormal(
+                    "mu_exp",
+                    mu=np.mean(data, axis=(0, 1)),
+                    sigma=upper_bounds[label] - lower_bounds[label],
                     lower=lower_bounds[label],
                     upper=upper_bounds[label],
-                    observed=data[j, :],
+                    shape=(n_exp,)
                 )
-            theta_stack = pm.TruncatedNormal(
-                f"theta_stack",
-                mu=mu_stack,
-                sigma=sigma_stack,
-                lower=lower_bounds[label],
-                upper=upper_bounds[label],
-                observed=data.reshape(-1, 1),
-            )
-            # Sample
-            trace = pm.sample(
-                draws=2000,
-                tune=2000,
-                initvals={
-                    'mu_exp': np.mean(data, axis=1),
-                    'sigma_exp': np.std(data, axis=1),
-                    'mu_stack': np.mean(data, axis=(0, 1)),
-                    'sigma_stack': np.std(data, axis=(0, 1)),
-                },
-                init='adapt_diag',
-                target_accept=0.9,
-            )
-            # Save
-            mu_exp_med = trace.posterior['mu_exp'].median(axis=(0, 1))
-            sigma_exp_med = trace.posterior['sigma_exp'].median(axis=(0, 1))
-            mu_stack_med = trace.posterior['mu_stack'].median()
-            sigma_stack_med = trace.posterior['sigma_stack'].median()
+                mu_stack = pm.TruncatedNormal(
+                    "mu_stack",
+                    mu=np.mean(data, axis=(0, 1)),
+                    sigma=upper_bounds[label] - lower_bounds[label],
+                    lower=lower_bounds[label],
+                    upper=upper_bounds[label]
+                )
+                sigma_exp = pm.HalfNormal("sigma_exp", 0.2, shape=(n_exp,))
+                sigma_stack = pm.HalfNormal("sigma_stack", 0.2)
+                # Likelihood
+                for j in range(n_exp):
+                    theta_exp = pm.TruncatedNormal(
+                        f"theta_exp_{j}",
+                        mu=mu_exp[j],
+                        sigma=sigma_exp[j],
+                        lower=lower_bounds[label],
+                        upper=upper_bounds[label],
+                        observed=data[j, :],
+                    )
+                theta_stack = pm.TruncatedNormal(
+                    f"theta_stack",
+                    mu=mu_stack,
+                    sigma=sigma_stack,
+                    lower=lower_bounds[label],
+                    upper=upper_bounds[label],
+                    observed=data.reshape(-1, 1),
+                )
+                # Sample
+                trace = pm.sample(
+                    draws=2000,
+                    tune=2000,
+                    initvals={
+                        'mu_exp': np.mean(data, axis=1),
+                        'sigma_exp': np.std(data, axis=1),
+                        'mu_stack': np.mean(data, axis=(0, 1)),
+                        'sigma_stack': np.std(data, axis=(0, 1)),
+                    },
+                    init='adapt_diag',
+                    target_accept=0.9,
+                )
+                # Save
+                mu_exp_med = trace.posterior['mu_exp'].median(axis=(0, 1))
+                sigma_exp_med = trace.posterior['sigma_exp'].median(axis=(0, 1))
+                mu_stack_med = trace.posterior['mu_stack'].median()
+                sigma_stack_med = trace.posterior['sigma_stack'].median()
 
-            mcmc_df.loc[mcmc_df.index[mcmc_df['obs_tag'] == obs_tag], label] = mu_exp_med
-            mcmc_df.loc[mcmc_df.index[mcmc_df['obs_tag'] == obs_tag], label + '_err'] = sigma_exp_med
-            stack_df.loc[obs_tag, label] = mu_stack_med
-            stack_df.loc[obs_tag, label + '_err'] = sigma_stack_med
+                mcmc_df.loc[mcmc_df.index[mcmc_df['obs_tag'] == obs_tag], label] = mu_exp_med
+                mcmc_df.loc[mcmc_df.index[mcmc_df['obs_tag'] == obs_tag], label + '_err'] = sigma_exp_med
+                stack_df.loc[obs_tag, label] = mu_stack_med
+                stack_df.loc[obs_tag, label + '_err'] = sigma_stack_med
 
-mcmc_df.to_hdf(mcmc_summary_file, 'individual')
-stack_df.to_hdf(mcmc_summary_file, 'stacked')
+    mcmc_df.to_hdf(mcmc_summary_file, 'individual')
+    stack_df.to_hdf(mcmc_summary_file, 'stacked')
