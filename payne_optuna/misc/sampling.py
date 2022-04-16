dd8ee845fd9bebc37c22a9c17e99e474b1c05ad3
nathansandford <nathan_sandford@berkeley.edu>
2022-04-16 10:35:01 -0700

    randdist

--- payne_optuna/misc/sampling.py
+++ payne_optuna/misc/sampling.py
@@ -144,3 +144,21 @@ class MCMCReader:
             elif label in ['rv']:
                 unscaled_samples[:, i] = self.emulator.rv_scale * unscaled_samples[:, i]
         return scaled_samples, unscaled_samples
+
+
+def randdist(x, pdf, nvals):
+    """Produce nvals random samples from pdf(x), assuming constant spacing in x."""
+    # get cumulative distribution from 0 to 1
+    cumpdf = np.cumsum(pdf)
+    cumpdf *= 1/cumpdf[-1]
+    # input random values
+    randv = np.random.uniform(size=nvals)
+    # find where random values would go
+    idx1 = np.searchsorted(cumpdf, randv)
+    # get previous value, avoiding division by zero below
+    idx0 = np.where(idx1==0, 0, idx1-1)
+    idx1[idx0==0] = 1
+    # do linear interpolation in x
+    frac1 = (randv - cumpdf[idx0]) / (cumpdf[idx1] - cumpdf[idx0])
+    randdist = x[idx0]*(1-frac1) + x[idx1]*frac1
+    return randdist
