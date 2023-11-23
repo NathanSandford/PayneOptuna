96aed349e3250a4d4c464e3f0dae5a184a330777
nathansandford <nathan_sandford@berkeley.edu>
2023-11-23 10:03:31 -0500

    DESI continuum fitting

--- payne_optuna/misc/desi.py
+++ payne_optuna/misc/desi.py
@@ -1,7 +1,11 @@
 from copy import deepcopy
+import numpy as np
+from numpy.polynomial import Polynomial
+from scipy.ndimage import percentile_filter
 import torch
 from torch.nested import nested_tensor
 from ..utils import ensure_tensor
+import matplotlib.pyplot as plt
 
 
 def pad_obs(obs):
@@ -65,3 +69,58 @@ def pad_obs(obs):
     else:
         obs_['blaz'] = None
     return obs_
+
+
+def gen_blaz(model, inst_res, obs, rv, prefit_cont_window=55, plot=True):
+    obs_wave = torch.vstack([ensure_tensor(wave, precision=torch.float64) for wave in obs['wave']])
+    obs_flux = torch.vstack([ensure_tensor(flux) for flux in obs['flux']])
+    obs_errs = torch.vstack([ensure_tensor(errs) for errs in obs['errs']])
+    obs_mask = torch.vstack([ensure_tensor(mask, precision=bool) for mask in obs['mask']])
+    obs_blaz = np.zeros_like(obs['wave'])
+
+    rv = rv * torch.ones((1, 1))
+    stellar_labels = torch.zeros((1, model.n_stellar_labels))
+    inst_res = inst_res * torch.ones((1, 1))
+    mod_flux, mod_errs = model(stellar_labels, rv, None, skip_cont=True)
+
+    footprint = np.concatenate(
+        [np.ones(prefit_cont_window), np.zeros(prefit_cont_window), np.ones(prefit_cont_window)]
+    )
+    tot_errs = torch.sqrt(mod_errs[0] ** 2 + obs_errs ** 2)
+    scaling = obs_flux / mod_flux[0]
+    scaling[~obs_mask] = 1.0
+    for i in range(model.n_obs_ord):
+        filtered_scaling = percentile_filter(scaling[i].detach().numpy(), percentile=25, footprint=footprint)
+        filtered_scaling = percentile_filter(filtered_scaling, percentile=75, footprint=footprint)
+        filtered_scaling[filtered_scaling <= 0.0] = 1.0
+        p = Polynomial.fit(
+            x=model.obs_norm_wave[i][obs_mask[i]].detach().numpy(),
+            y=filtered_scaling[obs_mask[i]],
+            deg=model.cont_deg,
+            w=(tot_errs[i] ** -1)[obs_mask[i]].detach().numpy(),
+            window=model.cont_wave_norm_range
+        )
+        obs_blaz[i] = p(model.obs_norm_wave[i]).detach().numpy()
+        if plot:
+            plt.figure(figsize=(20, 5))
+            plt.scatter(
+                obs_wave[i][obs_mask[i]].detach().numpy(),
+                obs_flux[i][obs_mask[i]].detach().numpy(),
+                c='k', marker='.', alpha=0.8
+            )
+            plt.plot(
+                obs_wave[i].detach().numpy(),
+                (mod_flux[0, i] * p(model.obs_norm_wave[i])).detach().numpy(),
+                c='r', alpha=0.8
+            )
+            plt.plot(
+                obs_wave[i].detach().numpy(),
+                obs_blaz[i],
+                c='b', alpha=0.8
+            )
+            plt.ylim(0, 1.5 * np.nanquantile(obs_flux[i][obs_mask[i]].detach().numpy(), 0.95))
+            plt.ylabel('Flux')
+            plt.xlabel('Wavelength')
+            plt.show()
+            plt.close('all')
+    return obs_blaz
