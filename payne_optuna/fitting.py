9990a7c383e993244df44235617baf743843b672
nathansandford <nathan_sandford@berkeley.edu>
2023-11-25 13:21:58 -0500

    troubleshooting walkers

--- payne_optuna/fitting.py
+++ payne_optuna/fitting.py
@@ -2017,12 +2017,12 @@ class PayneOptimizer:
 
     def neg_log_posterior(self, pred, target, pred_errs, target_errs):
         log_likelihood = gaussian_log_likelihood(pred, target, pred_errs, target_errs)
-        log_priors = torch.zeros_like(log_likelihood) #self.log_priors(log_likelihood)
+        log_priors = self.log_priors(log_likelihood)
         if torch.any(torch.isnan(log_likelihood)):
             raise RuntimeError('NaN value returned for log_likelihood')
         if torch.any(torch.isnan(log_priors)):
             raise RuntimeError('NaN value returned for log_priors')
-        return -1 * (log_likelihood + log_priors)
+        return -1 * (log_likelihood)# + log_priors)
 
     def neg_log_posterior_mixture(self, pred, target, pred_errs, target_errs, f_out, out_err_scale=50):
         log_likelihood_in = torch.log(1 - f_out) + gaussian_log_likelihood(
